5f7954f103a9caa47e749ce4534cecbc61283883
timzhao <timzhao@futunn.com>
2023-01-03 23:03:56 +0800

    ok

--- leNet/LeNet.py
+++ leNet/LeNet.py
@@ -1,9 +1,8 @@
 import matplotlib
 import torch
 from torch import nn
-from d2l import torch as d2l
+from utils import torch as torch_common
 import matplotlib.pyplot as plt
-
 
 net = nn.Sequential(
     nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
@@ -23,7 +22,7 @@ for layer in net:
 
 
 batch_size = 256
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+train_iter, test_iter = torch_common.load_data_fashion_mnist(batch_size=batch_size)
 
 
 def evaluate_accuracy_gpu(net, data_iter, device=None): #@save
@@ -33,7 +32,7 @@ def evaluate_accuracy_gpu(net, data_iter, device=None): #@save
         if not device:
             device = next(iter(net.parameters())).device
     # 正确预测的数量，总预测的数量
-    metric = d2l.Accumulator(2)
+    metric = torch_common.Accumulator(2)
     with torch.no_grad():
         for X, y in data_iter:
             if isinstance(X, list):
@@ -42,13 +41,13 @@ def evaluate_accuracy_gpu(net, data_iter, device=None): #@save
             else:
                 X = X.to(device)
             y = y.to(device)
-            metric.add(d2l.accuracy(net(X), y), y.numel())
+            metric.add(torch_common.accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
 
-d2l.set_figsize()
+torch_common.set_figsize()
 
-#@save
-def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+
+def train_LeNet(net, train_iter, test_iter, num_epochs, lr, device):
     """用GPU训练模型(在第六章定义)"""
     def init_weights(m):
         if type(m) == nn.Linear or type(m) == nn.Conv2d:
@@ -58,12 +57,12 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
     net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
     loss = nn.CrossEntropyLoss()
-    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+    animator = torch_common.Animator(xlabel='epoch', xlim=[1, num_epochs],
                             legend=['train loss', 'train acc', 'test acc'])
-    timer, num_batches = d2l.Timer(), len(train_iter)
+    timer, num_batches = torch_common.Timer(), len(train_iter)
     for epoch in range(num_epochs):
         # 训练损失之和，训练准确率之和，样本数
-        metric = d2l.Accumulator(3)
+        metric = torch_common.Accumulator(3)
         net.train()
         for i, (X, y) in enumerate(train_iter):
             timer.start()
@@ -74,7 +73,7 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
             l.backward()
             optimizer.step()
             with torch.no_grad():
-                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+                metric.add(l * X.shape[0], torch_common.accuracy(y_hat, y), X.shape[0])
             timer.stop()
             train_l = metric[0] / metric[2]
             train_acc = metric[1] / metric[2]
@@ -90,7 +89,7 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
     plt.show()
 
 lr, num_epochs = 0.9, 10
-train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+train_LeNet(net, train_iter, test_iter, num_epochs, lr, torch_common.try_gpu())
 
 
 
